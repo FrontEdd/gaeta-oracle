dfb14b6135612fbe9d127dede6cc8511ad0d9111
Edgar Razuri <edgar@urbaner.com>
2025-10-05 13:21:24 -0500

    Theme context and hero styling

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,49 +1,62 @@
 import styles from "./HeroStyles.module.css";
 import heroImg from "../../assets/hero-img.png";
-import themeIcon from "../../assets/sun.svg";
-import twitterIcon from "../../assets/twitter-light.svg";
-import githubIcon from "../../assets/github-light.svg";
-import linkedinIcon from "../../assets/linkedin-light.svg";
+import sun from "../../assets/sun.svg";
+import moon from "../../assets/moon.svg";
+import twitterLight from "../../assets/twitter-light.svg";
+import twitterDark from "../../assets/twitter-dark.svg";
+import githubLight from "../../assets/github-light.svg";
+import githubDark from "../../assets/github-dark.svg";
+import linkedinLight from "../../assets/linkedin-light.svg";
+import linkedinDark from "../../assets/linkedin-dark.svg";
 import CV from "../../assets/cv.pdf";
+import { useTheme } from "../../common/ThemeContext";
 
 function Hero() {
+  const { theme, toggleTheme } = useTheme();
+
+  const themeIcon = theme === "light" ? sun : moon;
+  const twitterIcon = theme === "light" ? twitterLight : twitterDark;
+  const githubIcon = theme === "light" ? githubLight : githubDark;
+  const linkedinIcon = theme === "light" ? linkedinLight : linkedinDark;
+
   return (
     <section id="hero" className={styles.container}>
       <div className={styles.colorModeContainer}>
-        <img className={styles.hero} src={heroImg} alt="hero-img" />
-        <img className={styles.colorMode} src={themeIcon} alt="theme-icon" />
+        <img
+          src={heroImg}
+          className={styles.hero}
+          alt="Profile picture of Harris Johnsen"
+        />
+        <img
+          className={styles.colorMode}
+          src={themeIcon}
+          alt="Color mode icon"
+          onClick={toggleTheme}
+        />
       </div>
-      <div>
+      <div className={styles.info}>
         <h1>
           Edgar
           <br />
           Razuri
         </h1>
-        <h2>Frontend Devoloper</h2>
+        <h2>Frontend Developer</h2>
         <span>
-          <a href="https://x.com/erazuris" target="_blank" rel="noreferrer">
+          <a href="https://x.com/erazuris" target="_blank">
             <img src={twitterIcon} alt="Twitter icon" />
           </a>
-          <a
-            href="https://github.com/FrontEdd"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src={githubIcon} alt="github icon" />
+          <a href="https://github.com/FrontEdd" target="_blank">
+            <img src={githubIcon} alt="Github icon" />
           </a>
-          <a
-            href="https://www.linkedin.com/in/edgarazuris/"
-            target="_blank"
-            rel="noreferrer"
-          >
+          <a href="https://www.linkedin.com/in/edgarazuris/" target="_blank">
             <img src={linkedinIcon} alt="Linkedin icon" />
           </a>
         </span>
-        <p>
+        <p className={styles.description}>
           With a passion for developing modern React web apps for commercial
-          businesses
+          businesses.
         </p>
-        <a href={CV} target="_blank" download>
+        <a href={CV} download>
           <button className="hover">Resume</button>
         </a>
       </div>
