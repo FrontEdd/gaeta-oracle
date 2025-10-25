2d89efe85a6049c32c522eeff2dac02d7fb63b21
Edgar Razuri <edgar@urbaner.com>
2025-10-25 11:30:57 -0500

    Sorting data into skills list format

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -3,6 +3,7 @@ import checkMarkIconDark from "../../assets/checkmark-dark.svg";
 import checkMarckIconLight from "../../assets/checkmark-light.svg";
 import SkillList from "../../common/SkillList";
 import { useTheme } from "../../common/ThemeContext";
+import { skillGroups } from "../../config/format.config";
 
 import styles from "./SkillsStyles.module.css";
 
@@ -10,12 +11,6 @@ function Skills() {
   const { theme } = useTheme();
   const checkMarkIcon =
     theme === "light" ? checkMarckIconLight : checkMarkIconDark;
-
-  const skillGroups = [
-    ["HTML", "CSS", "JavaScript", "TypeScript", "Node"],
-    ["React", "SQL", "Python", "Tailwind CSS"],
-    ["Redux", "Webpack", "Git", "Figma", "Miro"],
-  ];
 
   return (
     <section id="skills" className={styles.container}>
